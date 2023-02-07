b4c4fa9a4fced9b2e269c679de417086f959c84a
Onitsiky <hei.onitsiky@gmail.com>
2023-02-07 21:50:22 +0300

    chore: add tests for non-empty column

--- src/Tests/Column.test.js
+++ src/Tests/Column.test.js
@@ -1,6 +1,8 @@
 import renderer from "react-test-renderer";
 import {render, screen} from "@testing-library/react";
 import Column from "../Components/Column/Column";
+import Item from "../Components/Item/Item";
+import userEvent from "@testing-library/user-event";
 
 it("Render an empty column", () => {
     const component = renderer.create(
@@ -14,3 +16,24 @@ it("Render an empty column", () => {
     expect(screen.getByTestId("column-title")).toHaveTextContent("Test");
     expect(screen.getByTestId("column-elements")).toBeEmptyDOMElement();
 });
+
+it("Render column with an item", () => {
+    const component = renderer.create(
+        <Column title="Test with checkbox">
+            <Item checkbox={true} label="task 1" index={1}/>
+        </Column>
+    )
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    render(
+        <Column title="Test with checkbox">
+            <Item checkbox={true} label="task 1" index={1}/>
+        </Column>
+    );
+    const itemInput = screen.getByTestId("item-input");
+    expect(screen.getByTestId("column-title")).toHaveTextContent("Test with checkbox");
+    expect(screen.getByText("task 1")).toHaveTextContent("task 1");
+    userEvent.click(itemInput);
+    expect(itemInput).toBeChecked();
+})
